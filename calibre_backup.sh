2431b31a819738814f381216ccfd3be3b7d173ca
Volokzhanin Vadim <volokzhanin@yandex.ru>
2021-11-16 00:50:13 +1000

    raspberry

--- calibre_backup.sh
+++ calibre_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Создаем резервную копию
-cd /mnt/0/documents/projects/pc_settings/calibre
-tar cvpzf /mnt/0/backup/vvy_calibre/"$(date '+%Y-%m-%d').tar.gz" ./
+cd /home/ubuntu/docker/calibre/
+tar cvpzf /mnt/backup/backup/vvy_calibre/"$(date '+%Y-%m-%d').tar.gz" ./
 # Удаляем архивы резервной копии старше n дней
-find /mnt/0/backup/vvy_calibre/ -mtime +3 -type f -delete
+find /mnt/backup/backup/vvy_calibre/ -mtime +3 -type f -delete
 
 # restore 
-# cd /mnt/0/backup/vvy_calibre & tar xpvzf /mnt/0/backup/vvy_calibre/2021-10-09.tar.gz        
+# cd /home/pi/docker/calibre/ & tar xpvzf /mnt/backup/backup/vvy_calibre/2021-10-09.tar.gz
 
 
