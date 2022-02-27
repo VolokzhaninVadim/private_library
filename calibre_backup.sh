965ea3fdb2519f18e630149bcd17b42f98335abe
Volokzhanin Vadim <volokzhanin@yandex.ru>
2022-02-27 10:03:18 +0000

    path volokzhanin

--- calibre_backup.sh
+++ calibre_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Создаем резервную копию
-cd /home/ubuntu/docker/calibre/
+cd /home/volokzhanin/docker/calibre/
 tar cvpzf /mnt/backup/backup/vvy_calibre/"$(date '+%Y-%m-%d').tar.gz" ./
 # Удаляем архивы резервной копии старше n дней
 find /mnt/backup/backup/vvy_calibre/ -mtime +3 -type f -delete
 
 # restore 
-# cd /home/ubuntu/docker/calibre/ & tar xpvzf /mnt/backup/backup/vvy_calibre/2021-10-09.tar.gz
+# cd /home/volokzhanin/docker/calibre/ & tar xpvzf /mnt/backup/backup/vvy_calibre/2021-10-09.tar.gz
 
 
